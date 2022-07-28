82d038b5bf3219bd4b1233ba5e30dd8fe82d587c
Vasila Rakhmonkulova <vrakhmonkulova@gmail.com>
2022-07-27 22:01:45 -0400

    Changed the runner class

--- src/test/java/tiger/api/runner/review/TestRunner.java
+++ src/test/java/tiger/api/runner/review/TestRunner.java
@@ -2,7 +2,7 @@ package tiger.api.runner.review;
 
 import com.intuit.karate.junit5.Karate;
 
-public class RunnerReview {
+public class TestRunner {
 	@Karate.Test
 	public Karate RunTest() {
         return Karate.run("classpath:features").tags("EndToEnd");
